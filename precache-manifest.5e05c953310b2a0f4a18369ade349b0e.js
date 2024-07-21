7b6dc9f4992f197cf07962c9cf8b092e626bd756
akashpandey9956 <129768903+akashpandey9956@users.noreply.github.com>
2024-07-21 09:51:24 +0530

    Updates

---
+++ precache-manifest.5e05c953310b2a0f4a18369ade349b0e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8543dc09c327c346148aaf59cc596bba",
+    "url": "/TextUtills/index.html"
+  },
+  {
+    "revision": "8fabef6d4e3698ebdd4e",
+    "url": "/TextUtills/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "8dcd6d4017260ce4b576",
+    "url": "/TextUtills/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "8fabef6d4e3698ebdd4e",
+    "url": "/TextUtills/static/js/2.c65126d8.chunk.js"
+  },
+  {
+    "revision": "6cb2abd69bcf5c1bc8ea13bc6a30ce0e",
+    "url": "/TextUtills/static/js/2.c65126d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2973f7641095b5c901c1",
+    "url": "/TextUtills/static/js/3.ecd4ba8b.chunk.js"
+  },
+  {
+    "revision": "8dcd6d4017260ce4b576",
+    "url": "/TextUtills/static/js/main.86d4ab5b.chunk.js"
+  },
+  {
+    "revision": "fad2051d50d39c9c884a",
+    "url": "/TextUtills/static/js/runtime-main.e7bef066.js"
+  }
+]);
